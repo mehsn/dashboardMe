d2783de6a50ab382b7c2132ca35a8eb0918f12a2
mehsn <ihsnun@gmail.com>
2021-10-12 11:09:16 +0330

    commit one

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import Routes from "./Routes";
+import {connect, Provider} from "react-redux";
 import './App.css';
+import './css/react_animate_css.css';
+import './css/bootstrap.min.css';
+import {changeDesignAction, lightDesignAction, darkDesignAction, randomDesignAction} from "./App/Reducer/designReducer"
+import BaseCss from "./Components/BaseCss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+function App(props) {
+
+
+    function changeToDark() {
+        console.log("hi");
+    }
+
+    return (
+        <div className="App">
+            <BaseCss/>
+            <div className="hsd bgColor " > sdf </div>
+            <Routes>
+
+            </Routes>
+            <br/>
+            <div style={{
+                backgroundColor: props.designState.bgColor,
+                color: props.designState.textBaseColor,
+                textShadow: "1px 1px 1px" + props.designState.designColor
+            }}>
+                {props.designState.bgColor} salam
+            </div>
+
+            <button onClick={() => props.changeDesignAction({
+                designColor: "#313",
+                bgColor: "#099",
+                textBaseColor: "#492",
+            })}> costume
+            </button>
+            <button onClick={() => props.lightDesignAction()}> light theme</button>
+            <button onClick={() => props.darkDesignAction()}> dark theme</button>
+            <button onClick={() => props.randomDesignAction()}> random</button>
+        </div>
+
+    );
 }
 
-export default App;
+function mapStateToProps(storeState) {
+    console.log(storeState.designStore.design);
+    return {designState: storeState.designStore.design}
+}
+
+export default connect(mapStateToProps,
+    {changeDesignAction, lightDesignAction, darkDesignAction, randomDesignAction})(App);
